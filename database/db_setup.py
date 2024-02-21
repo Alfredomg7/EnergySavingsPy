7f025534f2c7f85daf392582a2d7c0fded073a3a
Alfredomg7 <alfredomg4000@gmail.com>
2024-02-20 21:51:02 -0800

    Added PDBT rate csv file, and implemented table creation and population in db_setup module

--- database/db_setup.py
+++ database/db_setup.py
@@ -33,7 +33,22 @@ def populate_solar_hours(conn, solar_hours_csv):
     except Exception as e:
         print(f"An error occurred: {e}")
 
-def setup_database(db_path=config.DATABASE_PATH, locations_csv='data/locations.csv', solar_hours_csv='data/solar_hours.csv'):
+def populate_pdbt_rate(conn, pdbt_rate_csv):
+    try:
+        with open(pdbt_rate_csv, 'r') as file:
+            csv_reader = csv.DictReader(file)
+            for row in csv_reader:
+                conn.execute('''INSERT INTO pdbt_rate (billing_period, transmission, distribution, cenace, supplier, services, energy, capacity)
+                                VALUES (?, ?, ?, ?, ?, ?, ?, ?)''',
+                             (row['billing_period'], row['transmission'], row['distribution'], row['cenace'], row['supplier'], row['services'], row['energy'], row['capacity']))
+    except sqlite3.DatabaseError as e:
+        print(f"Database error: {e}")
+    except FileNotFoundError:
+        print(f"File not found: {pdbt_rate_csv}")
+    except Exception as e:
+        print(f"An error occurred: {e}")
+
+def setup_database(db_path=config.DATABASE_PATH, locations_csv='data/locations.csv', solar_hours_csv='data/solar_hours.csv', pdbt_rate_csv='data/pdbt_rate.csv'):
     os.makedirs(os.path.dirname(db_path), exist_ok=True)
     
     try:
@@ -57,23 +72,33 @@ def setup_database(db_path=config.DATABASE_PATH, locations_csv='data/locations.c
                     FOREIGN KEY (location_id) REFERENCES locations (location_id)
                 )
             ''')
+            
+            # Create the pdbt_rate table
+            conn.execute('''
+                CREATE TABLE IF NOT EXISTS pdbt_rate(
+                    billing_period_id INTEGER PRIMARY KEY,
+                    billing_period TEXT NOT NULL,
+                    transmission REAL,
+                    distribution REAL,
+                    cenace REAL,
+                    supplier REAL,
+                    services REAL,
+                    energy REAL,
+                    capacity REAL,
+                    UNIQUE(billing_period),
+                    CHECK (billing_period LIKE '____-__')
+                )
+            ''')
 
             # Populate tables
             populate_locations(conn, locations_csv)
             populate_solar_hours(conn, solar_hours_csv)
-
+            populate_pdbt_rate(conn, pdbt_rate_csv)
             conn.commit()
     except sqlite3.DatabaseError as e:
         print(f"Database error: {e}")
     except Exception as e:
         print(f"An error occurred during setup: {e}")
 
-def initialize_database():
-    db_path = 'data/SolarData.db'
-    locations_csv = 'data/locations.csv'
-    solar_hours_csv = 'data/solar_hours.csv'
-
-    setup_database(db_path, locations_csv, solar_hours_csv)
-
 if __name__ == '__main__':
-    initialize_database()
+    setup_database()
