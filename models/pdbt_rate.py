2a592571d0fdc75ac3e2d9c145518f7e5e064c04
Alfredomg7 <alfredomg4000@gmail.com>
2024-03-06 09:16:13 -0800

    Rename 'rates' attribute to 'charges' in pdbt_rate model and refine encapsulation by making the attributes private

--- models/pdbt_rate.py
+++ models/pdbt_rate.py
@@ -5,28 +5,27 @@ class PdbtRate:
     IVA_RATE = 1.08
 
     def __init__(self, state, end_month, pdbt_rate_data=None):
-        self.pdbt_rate_data = pdbt_rate_data or PdbtRateData()
-        self.rates = self.pdbt_rate_data.get_charges(state, calculate_start_month(end_month), end_month)
+        self._pdbt_rate_data = pdbt_rate_data or PdbtRateData()
+        self._charges = self._pdbt_rate_data.get_charges(state, calculate_start_month(end_month), end_month)
     
     def calculate_monthly_payments(self, monthly_consumption):
-        if not self.rates or len(self.rates) != 12:
+        if not self._charges or len(self._charges) != 12:
             raise ValueError("Rates data is incomplete or not available. Please fetch correct rates before calculating payments.")
         
         if len(monthly_consumption) != 12:
             raise ValueError("Monthly consumptions list must contain 12 items, one for each month.")
 
-        return [self._calculate_payment(rate, consumption) for rate, consumption in zip(self.rates, monthly_consumption)]
+        return [self._calculate_payment(rate, consumption) for rate, consumption in zip(self._charges, monthly_consumption)]
         
-    def _calculate_payment(self, rate, consumption):
+    def _calculate_payment(self, charge, consumption):
         cost_components = [
-            consumption * rate["transmission"],
-            consumption * rate["distribution"],
-            consumption * rate["cenace"],
-            rate["supplier"],
-
-            consumption * rate["services"],
-            consumption * rate["energy"],
-            consumption * rate["capacity"],
+            consumption * charge["transmission"],
+            consumption * charge["distribution"],
+            consumption * charge["cenace"],
+            charge["supplier"],
+            consumption * charge["services"],
+            consumption * charge["energy"],
+            consumption * charge["capacity"],
         ]
         total_cost = sum(cost_components) * self.IVA_RATE
         return round(total_cost, 2)
--- tests/test_pdbt_rate_model.py
+++ tests/test_pdbt_rate_model.py
@@ -7,11 +7,11 @@ class TestPdbtRate(unittest.TestCase):
         self.state = 'Baja California'
         self.end_month = '2022-12'
 
-        self.mock_rates = [{'transmission': 0.1, 'distribution': 0.05, 'cenace': 0.01,
+        self.mock_charges = [{'transmission': 0.1, 'distribution': 0.05, 'cenace': 0.01,
                             'supplier': 100, 'services': 0.02, 'energy': 0.07, 'capacity': 0.03} for _ in range(12)]
 
     def test_calculate_monthly_payments(self):
-        with patch('models.pdbt_rate.PdbtRateData.get_charges', return_value=self.mock_rates):
+        with patch('models.pdbt_rate.PdbtRateData.get_charges', return_value=self.mock_charges):
             pdbt_rate = PdbtRate(self.state, self.end_month)
 
             monthly_consumptions = [100, 150, 200, 250, 300, 350, 400, 450, 500, 550, 600, 650]
